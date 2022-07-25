31c6106f8a702f0845bdf577172bce7a5655419f
raheelbaig <68862554+raheelbaig@users.noreply.github.com>
2022-07-25 14:21:23 -0700

    server
    weather server deploy

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+const app = express()
+app.use(bodyParser.json())
+const port = process.env.PORT || 3000
+
+app.post('/dialogflow-fulfillment', (request, response) => {
+    dialogflowFulfillment(request, response)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+const dialogflowFulfillment = (request, response) => {
+    const agent = new WebhookClient({ request, response })
+
+    function welcome(agent) {
+        agent.add(`Welcome to my agent!`);
+    }
+
+    function fallback(agent) {
+        agent.add(`I didn't understand`);
+        agent.add(`I'm sorry, can you try again?`);
+    }
+
+    function weatherHandler(agent) {
+
+        agent.add("Hello, this was a nice tutorial by axlewebtech")
+    }
+
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('Weather', weatherHandler);
+    agent.handleRequest(intentMap);
+
+}
